4ab64bd386f7e1e2a5310ab54c097fb0460eb874
Nathan099-dev <tonininathan8@gmail.com>
2024-11-21 13:51:03 -0300

    criação do js

--- CADASTRO-FUNGITECH/tela-cadastro/validação.js
+++ CADASTRO-FUNGITECH/tela-cadastro/validação.js
@@ -1,28 +1,55 @@
-var nome_usuario = nome.value;
-var idade_usuario = idade.value;
-var cidade_usuario = cidade.value;
-var telefone_usuario = telefone.value;
-var email_usuario = email.value;
-
 function valida_campo(){
-    if (nome_usuario == null) {
-        nome_usuario.style.borderColor = 'red';
+    const nomeUsuario = nome.value;
+    const idadeUsuario = idade.value;
+    const cidadeUsuario = cidade.value;
+    const telefoneUsuario = telefone.value;
+    const emailUsuario = email.value;
+    const senhaUsuario = email.value;
 
-    }if (idade_usuario == null) {
-        idade_usuario.style.borderColor = 'red';
-
-    }if (cidade_usuario == null) {
-        cidade_usuario.style.borderColor = 'red';
-
-    }if (telefone_usuario == null) {
-        telefone_usuario.style.borderColor = 'red';
-
-    }if (email_usuario == null) {
-        email_usuario.style.borderColor = 'red';
-
-    }if (email_usuario.indexOf('@') == -1 || email_usuario.indexOf('.') == -1) {
-        email.usuario.style.borderColor = 'red';
-        
+    if (nomeUsuario.value == '') {
+        window.alert('Campo nome, inválido');
+         document.getElementById('nome').style.border = '1px solid red';
     }
-}
 
+    if (idadeUsuario.value == '') {
+        window.alert('Campo idade, inválido');
+        document.getElementById('idade').style.border = '1px solid red';
+    }
+        
+    if (cidadeUsuario.value == '') {
+        window.alert('Campo cidade, inválido');
+        document.getElementById('cidade').style.border = 'px solid red';
+    }
+
+    if (telefoneUsuario.value == '') {
+        window.alert('Campo telefone, inválido');
+        document.getElementById('telefone').style.border = '1px solid  red';
+    }
+
+    if (emailUsuario.value == '') {
+        window.alert('Campo email, inválido');
+        document.getElementById('email').style.border = '1px solid red';
+
+    }
+
+    if (emailUsuario.indexOf('@').value == '' || emailUsuario.indexOf('.').value == '') {
+        window.alert('Este endereço de email não é válido');
+        document.getElementById('email').style.border = '1px solid  red'
+
+    }
+        
+    if (senhaUsuario.value == '') {
+        window.alert('Campo senha, inválido');
+        document.getElementById('senha').border = '1px solid red';
+    }
+
+    if (senhaUsuario.length < 6) {
+        window.alert('Sua senha precisa ter, pelo menos, 6 caracteres');
+        document.getElementById('senha').style.border = 'px solid red'
+        
+    } else {
+        document.querySelectorAll('input').style.border = 'green';
+
+    }
+   
+}
